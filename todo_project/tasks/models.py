f6c060b3f021a5a26dc75c166a15c90941a034ca
Kushal Yadav <ky1368177@gmail.com>
2025-03-18 09:07:03 +0000

    created user model

--- todo_project/tasks/models.py
+++ todo_project/tasks/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    pass 
+
+
 class tasks(models.Model):
     task_name = models.CharField(max_length=20)
     complete = models.BooleanField(default=False)

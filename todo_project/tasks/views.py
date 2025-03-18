1f78c4ef10fccff7c9f9fc7c13671e53cc8e6a08
Kushal Yadav <ky1368177@gmail.com>
2025-03-18 09:33:25 +0000

    edited views

--- todo_project/tasks/views.py
+++ todo_project/tasks/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.contrib import messages
 # Create your views here.
+def home(request):
+    pass
+
+def task(request):
+    pass
+
+def add_task(request):
+    pass
+
+def edit_task(request):
+    pass
+
+def delete_task(request):
+    pass
+
+def completed(request):
+    pass

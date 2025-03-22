20fad250a19c404422e40518ae4f250db35adfb9
Kushal Yadav <ky1368177@gmail.com>
2025-03-22 18:31:47 +0000

    created SignUp model

--- todo_project/auth_app/models.py
+++ todo_project/auth_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class SignUp(models.Model):
+    username = models.CharField(max_length=12)
+    email = models.EmailField(max_length=256)
+    password = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.username
